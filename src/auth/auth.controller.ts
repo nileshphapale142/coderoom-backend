8416f0264892ca6a3b3798323ee8c580c63a79d3
Nilesh Phapale <nileshphapale142@gmail.com>
2024-07-28 16:29:23 +0530

    Cookies updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthProvider } from './auth.service';
-import { SignUpDto, SignInDto } from './dto';
+import { SignInDto, SignUpDto } from './dto';
+
 
 @Controller('/auth')
 export class AuthController {
@@ -10,19 +11,23 @@ export class AuthController {
   @Post('/signup')
   async signUp(@Body() dto: SignUpDto, @Res({passthrough: true}) response: Response) {
     const access_token = await this.authProvider.signUp(dto);
-    
-    const expiryDate = new Date()
-    expiryDate.setDate(expiryDate.getDate() + 30)
-    
+
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 30);
+
     response.cookie('access_token', access_token.access_token, {
       httpOnly: true,
       secure: true,
+      sameSite: 'none',
+      domain: 'https://coderoom-backend.onrender.com',
       expires: expiryDate
     })
 
     response.cookie('is_teacher', dto.isTeacher, {
       httpOnly: false,
       secure: true,
+      sameSite: 'none',
+      domain: 'https://coderoom-backend.onrender.com',
       expires: expiryDate
     })
     return access_token
@@ -32,22 +37,26 @@ export class AuthController {
   async signIn(@Body() dto: SignInDto, @Res({passthrough: true}) response: Response) {
     const {at, isTeacher } =  await this.authProvider.signIn(dto);
     const { access_token } = at
-    
+
     const expiryDate = new Date()
     expiryDate.setDate(expiryDate.getDate() + 30)
-        
+
     response.cookie('access_token', access_token, {
       httpOnly: true,
       secure: false,
+      sameSite: 'none',
+      domain: 'https://coderoom-backend.onrender.com',
       expires: expiryDate
     })
-    
+
     response.cookie('is_teacher', isTeacher, {
       httpOnly: false,
       secure: false,
+      sameSite: 'none',
+      domain: 'https://coderoom-backend.onrender.com',
       expires: expiryDate
     })
-    
+
     return access_token
   }
 }
