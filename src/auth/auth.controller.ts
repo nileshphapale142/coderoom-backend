25672b26e336706c0da3b3d56a0b2c5e65216b02
Nilesh Phapale <nileshphapale142@gmail.com>
2024-07-29 16:49:51 +0530

    cookie upadate (hope so last)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,62 +6,61 @@ import { SignInDto, SignUpDto } from './dto';
 
 @Controller('/auth')
 export class AuthController {
+  
   constructor(private authProvider: AuthProvider) {}
+  
+  cookieOptions = {
+    secure: false,
+    sameSite: 'lax' as boolean | 'none' | 'lax' | 'strict',
+    partitioned: true,
+  };
 
   @Post('/signup')
   async signUp(@Body() dto: SignUpDto, @Res({passthrough: true}) response: Response) {
     const access_token = await this.authProvider.signUp(dto);
 
-    const expiryDate = new Date();
-    expiryDate.setDate(expiryDate.getDate() + 30);
+    // const expiryDate = new Date();
+    // expiryDate.setDate(expiryDate.getDate() + 30);
 
-    response.cookie('access_token', access_token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none',
-        partitioned: true,
-        expires: expiryDate
-      });
+    // response.cookie('access_token', access_token, {
+    //     ...this.cookieOptions,
+    //     httpOnly: true,
+    //     expires: expiryDate
+    //   });
     
-      response.cookie('is_teacher', dto.isTeacher, {
-        httpOnly: false,
-        secure: true,
-        sameSite: 'none',
-        partitioned: true,
-        expires: expiryDate
-      });
+    //   response.cookie('is_teacher', dto.isTeacher, {
+    //     ...this.cookieOptions,
+    //     httpOnly: false,
+    //     expires: expiryDate
+    //   });
     
     response.header('Content-Type', 'application/json');
-    return access_token
+    return { access_token: access_token.access_token, isTeacher: dto.isTeacher };
   }
 
   @Post('/signin')
   async signIn(@Body() dto: SignInDto, @Res({passthrough: true}) response: Response) {
     const {at, isTeacher } =  await this.authProvider.signIn(dto);
-    const { access_token } = at
+    const { access_token } = at;
 
-    const expiryDate = new Date()
-    expiryDate.setDate(expiryDate.getDate() + 30)
+    // const expiryDate = new Date()
+    // expiryDate.setDate(expiryDate.getDate() + 30)
 
-    response.cookie('access_token', access_token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none',
-        partitioned: true,
-        expires: expiryDate
-      });
+    // response.cookie('access_token', access_token, {
+    //     ...this.cookieOptions,
+    //     httpOnly: true,
+    //     expires: expiryDate
+    //   });
     
-      response.cookie('is_teacher', isTeacher, {
-        httpOnly: false,
-        secure: true,
-        sameSite: 'none',
-        partitioned: true,
-        expires: expiryDate
-      });
+    //   response.cookie('is_teacher', isTeacher, {
+    //     ...this.cookieOptions,
+    //     httpOnly: false,
+    //     expires: expiryDate
+    //   });
     
     response.header('Content-Type', 'application/json');
+    
 
-
-    return access_token
+    return { access_token, isTeacher };
   }
 }
