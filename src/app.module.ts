1e3a815f3b3c6de36186d463388e66c365d6a7d9
Nilesh Phapale <nileshphapale142@gmail.com>
2024-11-11 19:20:16 +0530

    Gemini module Import error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,7 @@ import { UserModule } from './user/user.module';
 import { QuestionModule } from './question/question.module';
 import { QuestionController } from './question/question.controller';
 import { SubmissionModule } from './submission/submission.module';
-import { GeminiModule } from './gemini/gemini.module';
 import { Judge0Module } from './judge0/judge0.module';
-import { AdminProvider } from './admin/admin.service';
 import { AdminController } from './admin/admin.controller';
 import { AdminModule } from './admin/admin.module';
 
@@ -28,7 +26,6 @@ import { AdminModule } from './admin/admin.module';
     UserModule,
     QuestionModule,
     SubmissionModule,
-    GeminiModule,
     Judge0Module,
     AdminModule,
   ],
