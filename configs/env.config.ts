21f32ad8357c45af5b50298c007eaebfe14b34d7
Nilesh Phapale <nileshphapale142@gmail.com>
2024-11-11 19:17:53 +0530

    Gemini removed

--- configs/env.config.ts
+++ configs/env.config.ts
@@ -3,17 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  GEMINI: {
-    KEY: process.env.GEMINI_API_KEY || '',
-    PRO_MODEL: process.env.GEMINI_PRO_MODEL || 'gemini-pro',
-    FLASH_MODEL: process.env.GEMINI_FLASH_MODEL || 'gemini-1.5-flash',
-  },
   JUDGE0: {
-    // KEY: process.env.X_RAPIDAPI_KEY || '',
-    // HOST: process.env.X_RAPIDAPI_HOST || ''
-    
-    KEY: process.env.SULU_API_KEY || '',
-    HOST: process.env.SULU_API_HOST || ''
+    KEY: process.env.JUDGE0_API_KEY || '',
+    HOST: process.env.JUDGE0_API_HOST || ''
   },
   
   ADMIN: {
